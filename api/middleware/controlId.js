a371450b1711240ac8a6cf4d4009035eec7f2838
Taha Şahin <115729027+tahasahinn@users.noreply.github.com>
2025-10-18 19:39:46 +0300

    Update controlId.js

--- api/middleware/controlId.js
+++ api/middleware/controlId.js
@@ -3,7 +3,7 @@ import { readRecipes } from "../model/recipeModel.js";
 const data = readRecipes();
 
 const controlId = (req, res, next) => {
-  const found = data.find((i) => i.id === req.params.id);s
+  const found = data.find((i) => i.id === req.params.id);
 
   if (!found) {
     return ress
